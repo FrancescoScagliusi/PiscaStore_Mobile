82f7c8cfcc3341a334fe02e829b4c8f0cbc54c88
FRANK <frank.sca98@gmail.com>
2022-03-21 18:04:38 +0100

    login register

--- PiscaStore/app/src/main/java/it/unito/piscastore/view/activity/LandingActivity.kt
+++ PiscaStore/app/src/main/java/it/unito/piscastore/view/activity/LandingActivity.kt
@@ -1,14 +1,32 @@
 package it.unito.piscastore.view.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import it.unito.piscastore.R
+import kotlinx.android.synthetic.main.activity_landing.*
 
 class LandingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+        btnLoginLanding.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    fun register(view : View){
+        intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun login(view: View){
+        intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
--- PiscaStore/app/src/main/java/it/unito/piscastore/view/fragment/LoginFragment.kt
+++ PiscaStore/app/src/main/java/it/unito/piscastore/view/fragment/LoginFragment.kt
@@ -1,11 +1,24 @@
 package it.unito.piscastore.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import it.unito.piscastore.MainActivity
 import it.unito.piscastore.R
+import it.unito.piscastore.controller.UserService
+import it.unito.piscastore.model.CurrentUser
+import it.unito.piscastore.model.SigninUser
+import it.unito.piscastore.view.activity.LandingActivity
+import it.unito.piscastore.view.activity.RegisterActivity
+import kotlinx.android.synthetic.main.fragment_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -18,5 +31,41 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnLogin.setOnClickListener{
+
+            val retrofit = Retrofit.Builder()
+                    .baseUrl(resources.getString(R.string.url_user))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            val service = retrofit.create(UserService::class.java)
+            val user = SigninUser(txtUsername.text.toString(), txtPassword.text.toString())
+            val call = service.login(user)
+            println("Logging")
+            call?.enqueue(object : Callback<CurrentUser?> {
+                override fun onResponse(call: Call<CurrentUser?>, response: Response<CurrentUser?>) {
+                    val u = response.body()!!
+                    println("Logged: " + u)
+                    var intent = Intent(getContext(), MainActivity::class.java)
+                    intent.putExtra("token", u.accessToken)
+                    startActivity(intent)
+
+                }
+
+                override fun onFailure(call: Call<CurrentUser?>, t: Throwable) {
+                    println("ERROR: "+t.message.toString())
+                }
+            })
+        }
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(context, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBack.setOnClickListener {
+            val intent = Intent(context, LandingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
