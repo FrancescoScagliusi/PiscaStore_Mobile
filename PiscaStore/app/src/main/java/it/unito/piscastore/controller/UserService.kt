bd941366020028f074ea5e675fb422afdde6f15a
FRANK <frank.sca98@gmail.com>
2022-03-22 12:01:15 +0100

    fixed user

---
+++ PiscaStore/app/src/main/java/it/unito/piscastore/controller/UserService.kt
@@ -0,0 +1,15 @@
+package it.unito.piscastore.controller
+
+import it.unito.piscastore.model.*
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface UserService {
+    @POST("signin")
+    fun login(@Body user: SigninUser): Call<CurrentUser?>
+
+    @POST("signup")
+    fun register(@Body user: SignupUser?): Call<CurrentUser?>?
+}
---
+++ PiscaStore/app/src/main/java/it/unito/piscastore/model/User.kt
@@ -0,0 +1,27 @@
+package it.unito.piscastore.model
+
+data class SigninUser (
+    var username: String,
+    var password: String
+)
+
+data class SignupUser(
+        var name: String,
+        var surname: String,
+        var username: String,
+        var email: String,
+        var password: String,
+        val phone: String
+)
+
+data class CurrentUser(
+        var id: Long,
+        var name: String,
+        var surname: String,
+        var username: String,
+        var email: String,
+        var roles: Array<Any>,
+        var accessToken: String,
+        var tokenType: String
+)
+
